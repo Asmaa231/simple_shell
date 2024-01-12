88b24e7dab8b675410e320ad33402e91a6cd398d
Asmaa231 <asmaasalem231@gmail.com>
2024-01-12 06:43:42 -0800

    a

--- stringtok.c
+++ stringtok.c
@@ -8,36 +8,44 @@
 
 char **string_token(char *command, int read_chars)
 {
-	char whole_cmd, *cp_cmd = NULL;
-	const char *delim = " \n";
-	char *tok;
-	int toks_num = 0;
-	char **argv;
-	int i;
+	char *cp_cmd = NULL;
+	const char *delim = " ";
+	char *tok, **argv;
+	int toks_num = 0, i;
 
 	cp_cmd = malloc(sizeof(char) * read_chars + 1);
 	if (cp_cmd == NULL)
+	{
 		perror("memo allocation err");
-
+		exit(EXIT_FAILURE);
+	}
 	strcpy(cp_cmd, command);
 	tok = strtok(cp_cmd, delim);
-
 	while (tok)
 	{
 		toks_num++;
-		tok = strtok(0, delim);
+		tok = strtok(NULL, delim);
 	}
 	toks_num++;
-	argv = malloc(sizeof(char) * (toks_num + 1));
-	tok = strtok(cp_cmd, delim);
+	argv = malloc(sizeof(char) * toks_num);
+	if (argv == NULL)
+	{
+		perror("memory allocation error");
+		exit(EXIT_FAILURE);
+	}
+	tok = strtok(command, delim);
 	for (i = 0; tok != NULL; i++)
 	{
 		argv[i] = malloc(sizeof(char) * (strlen(tok) + 1));
+		if (argv[i] == NULL)
+		{
+			perror("memory allocation error");
+			exit(EXIT_FAILURE);
+		}
 		_strcpy(argv[i], tok);
 		tok = strtok(NULL, delim);
 	}
 	argv[i] = NULL;
 	free(cp_cmd);
-	free(command);
 	return (argv);
 }
