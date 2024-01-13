3207640c236dbcddaf4e0f03870d5c4ff8c32880
Mohamed <mieaa638@gmail.com>
2024-01-13 04:45:33 +0200

    string.c

--- string.c
+++ string.c
@@ -2,30 +2,29 @@
 
 /**
  *_strlen - retuns the length of a string
- *@e: the string which will be checked 
+ *@e: the string which will be checked
  *
  * Retrun: integer length of the string
  */
 int _strlen(char *e)
 {
-	int c= 0;
+	int c = 0;
 
-	if(!e)
+	if (!e)
 		return (0); 
-
 	while (*e != '\0')
 		c++;
 		e++;
 	return (c);
 }
 /**
- * _strcmp- performs to compare between two null terminated strings leographically
+ * _strcmp- compare between two null terminated strings leographically
  *@c1: The first string
  *@c2: the second string
  *
- * Return: negative if s1 < s2, positive if s1 > s2,  Zero if s1 == s2 
+ *Return: negative if s1 < s2, positive if s1 > s2,  Zero if s1 == s2
  */
-int _strcmp(char *c1 , char *c2)
+int _strcmp(char *c1, char *c2)
 {
 	while (*c1 && *c2)
 	{
@@ -35,7 +34,7 @@ int _strcmp(char *c1 , char *c2)
 		c2++;
 	}
 if (*c1 == *c2)
-	return(0);
+	return (0);
 else
 	return (*c1 < *c2 ? -1 : 1);
 }
@@ -45,12 +44,12 @@ else
  * @haystack: A string to look or search
  * @needle: The Substring that needs to be found 
  *
- * Return: address of next char of haystack or Null
+ *Return: address of next char of haystack or Null
  */
-char *starts_with(const char *haystack, const char *needle)
+char *starts_with (const char *haystack, const char *needle)
 {
-	while (*needle)
-		if(*needle++ != *haystack++)
-			return (NULL);
-	return ((char*)haystack);
+while (*needle)
+if(*needle++ != *haystack++)
+return (NULL);
+return ((char *)haystack);
 }
