53d1f7809154d4a30712858831ddef5bb1ab310c
Mohamed <mieaa638@gmail.com>
2024-01-15 02:46:44 +0200

    EXECV.c

--- EXECV.c
+++ EXECV.c
@@ -23,21 +23,23 @@ return (0);
 */
 int _myexit(info_t *intel)
 {
-int exit_status;
-if (intel->argv[1])
+int exit_status:
+if (intel->argv[1]) /* This is to find out if there is an exit argument*/
 {
- exit_status = atoi(intel->argv[1]);
-if (exit_status == 0) 
+exit_status = atoi(intel->argv[1]);
+if (exit_status == 0)
 {
-intel->status = -2;
+intel->status = -2; /*indicates the exit statemnet if it's illegal statement*/
+/*Assuming print_error and _eputs are defined and handle error printing
+_eputs are defined and handle error printing*/
 print_error(intel, "Illegal Number:");
 _eputs(intel->argv[1]);
 _eputchar("\n");
-return 1;
+return (1);/*Indicates an error condition*/
 }
 intel->errnum = exit_status;
-return exit_status;
+return (exit_status); /* Return the exit status*/
 }
-intel->errnum = -1;
-return -1;
+intel->errnum = -1 /* Unspecified error*/
+return (-1); /*Indicates an error condition*/
 }
